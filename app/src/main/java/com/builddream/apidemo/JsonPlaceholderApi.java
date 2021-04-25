7807a3efd6450a5b5389db09a0e5f9e9fee47e8f
Neha Ghedia <nsghedia@gmail.com>
2021-04-25 04:07:54 -0700

    Simple API Demo with ScrollView

---
+++ app/src/main/java/com/builddream/apidemo/JsonPlaceholderApi.java
@@ -0,0 +1,10 @@
+package com.builddream.apidemo;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceholderApi {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
